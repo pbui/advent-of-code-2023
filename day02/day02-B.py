33af33136b18fe50fd69716f15d2911abd570783
Peter Bui <pbui@bx612.space>
2023-12-02 07:52:37 -0500

    Day 02 - Part 2

---
+++ day02/day02-B.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+''' Day 02 - Part A '''
+
+from collections import defaultdict
+from typing      import Iterator
+import sys
+
+# Types
+
+Game = dict[str, int]
+
+# Constants
+
+RED_MAX   = 12
+GREEN_MAX = 13
+BLUE_MAX  = 14
+
+# Functions
+
+def read_game(stream=sys.stdin) -> Game:
+    try:
+        game_string, cubes_string = stream.readline().split(':')
+    except ValueError:
+        return {}
+
+    game: Game = defaultdict(int)
+    game['id'] = int(game_string.split()[-1])
+
+    for cubes in cubes_string.split(';'):
+        for cube in cubes.split(','):
+            count, color = cube.split()
+            game[color] = max(game[color], int(count))
+
+    return game
+
+def read_games(stream=sys.stdin) -> Iterator[Game]:
+    while game := read_game(stream):
+        yield game
+
+def power(game: Game) -> int:
+    return game['red'] * game['green'] * game['blue']
+
+# Main Execution
+
+def main(stream=sys.stdin) -> None:
+    sum_of_sets = sum(power(game) for game in read_games(stream))
+    print(sum_of_sets)
+
+if __name__ == '__main__':
+    main()
