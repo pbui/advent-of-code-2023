e9ac04bb73a3f7d0e1947e4ae4db12bbc2e60133
Peter Bui <pbui@nd.edu>
2023-12-05 15:57:53 -0500

    Day 05 - Part B

--- day05/day05-B.py
+++ day05/day05-B.py
@@ -2,7 +2,11 @@
 
 ''' Day 05 - Part A '''
 
-from typing import Iterator
+from collections import deque
+from typing      import Iterator
+
+import concurrent.futures
+import functools
 import itertools
 import re
 import sys
@@ -10,17 +14,18 @@ import sys
 # Types
 
 Seeds = list[int]
+Range = tuple[int, int]
 Map   = tuple[int, int, int]
 Maps  = list[Map]
 
 # Functions
 
 def batched(iterable, n):
-	if n < 1:
-		raise ValueError('n must be at least one')
-	it = iter(iterable)
-	while batch := tuple(itertools.islice(it, n)):
-		yield batch
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(itertools.islice(it, n)):
+        yield batch
 
 def read_almanac(stream=sys.stdin) -> tuple[Seeds, list[Map]]:
     seeds: Seeds = [int(seed) for seed in stream.readline().split(':')[-1].split()]
@@ -40,23 +45,29 @@ def read_almanac(stream=sys.stdin) -> tuple[Seeds, list[Map]]:
 
     return seeds, maps
 
-def locate_seed(srange: tuple[int, int], maps: list[Map]) -> int:
-    minimum = sys.maxsize
-    for location in range(srange[0], srange[0] + srange[1] + 1):
-        for amap in maps:
-            for dst, src, length in amap:
-                if src <= location <= src + length:
-                    location = dst + (location - src)
-                    break
-        minimum = min(location, minimum)
+def locate_seed(seed: int, maps: list[Map]={}) -> int:
+    location = seed
+    for amap in maps:
+        for dst, src, length in amap:
+            if src <= location < src + length:
+                location = dst + (location - src)
+                break
+    return location
 
-    return minimum
+def locate_seeds(srange: Range, maps: list[Map]={}) -> int:
+    seeds   = range(srange[0], srange[0] + srange[1])
+    locator = functools.partial(locate_seed, maps=maps)
+    return min(map(locator, seeds))
 
 # Main Execution
 
 def main(stream=sys.stdin) -> None:
     seeds, maps = read_almanac(stream)
-    locations   = [locate_seed(srange, maps) for srange in batched(seeds, 2)]
+    locator     = functools.partial(locate_seeds, maps=maps)
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        locations = executor.map(locator, batched(seeds, 2))
+
     print(min(locations))
 
 if __name__ == '__main__':
