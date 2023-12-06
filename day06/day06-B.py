ae3ce1b36cb9dad6df7fcaa718f6239800d6fb6f
Peter Bui <pbui@bx612.space>
2023-12-06 06:19:41 -0500

    Day 06 - Part B

---
+++ day06/day06-B.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+''' Day 06 - Part A '''
+
+import sys
+
+# Functions
+
+def race(charge_time, total_time):
+    return charge_time * (total_time - charge_time)
+
+# Main Execution
+
+def main(stream=sys.stdin) -> None:
+    time     = int(''.join(stream.readline().split(':')[-1].split()))
+    distance = int(''.join(stream.readline().split(':')[-1].split()))
+    ways     = [c for c in range(1, time + 1) if race(c, time) > distance]
+    print(len(ways))
+
+if __name__ == '__main__':
+    main()
